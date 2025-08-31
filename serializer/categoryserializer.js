7abd5f0e0e3987183c8a8d0c5a8c4e165c27f98e
Hammami Rayen <117598885+rayen-heroshima@users.noreply.github.com>
2025-08-31 13:13:07 +0100

    delete Lines

--- serializer/categoryserializer.js
+++ serializer/categoryserializer.js
@@ -3,11 +3,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const CategorySerializer = new JSONAPISerializer("categories", {
     id: "category_id",
     attributes: ["name", "description", "created_at", "updated_at", "sections"],
-    keyForAttribute: 'snake_case',
-    sections: {
-        ref: (section, category) => section.section_id,
-        attributes: ["title", "content"]
-    }
+    keyForAttribute: 'snake_case'
 });
 
 module.exports = CategorySerializer;
