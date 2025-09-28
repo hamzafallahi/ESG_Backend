a5155531c24b0e8cd426e5b5e39cdf89c7a7627a
hamzafallahi <hamzafallahi@esen.tn>
2025-09-28 10:17:58 +0100

    feat: Add SSL configuration for database connections to enforce security

--- config/app-config.js
+++ config/app-config.js
@@ -7,7 +7,13 @@ const defaultConfig = {
         name: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require: true,              // Enforce SSL
+                rejectUnauthorized: false   // Allow self-signed certs (Render usually needs this)
+            }
+        }
     },
     JWT_SECRET: process.env.JWT_SECRET || 'esg-secret-key',
     JWT_EXPIRATION: process.env.JWT_EXPIRATION || '24h',
@@ -45,6 +51,12 @@ module.exports = {
         host: process.env.DB_HOST ,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT ,
-        httpPort: process.env.SERVICE_PORT
+        httpPort: process.env.SERVICE_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,              // Enforce SSL
+                rejectUnauthorized: false   // Allow self-signed certs (Render usually needs this)
+            }
+        }
     },
 };
