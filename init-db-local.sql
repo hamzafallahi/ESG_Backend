d2ec8c3386c8274ed01b3a1f5ee0224b6aa77d5d
hamzafallahi <hamzafallahi@esen.tn>
2025-10-28 11:12:49 +0100

    fix: Update database configuration and initialization script for consistency

--- init-db-local.sql
+++ init-db-local.sql
@@ -1,4 +1,4 @@
 -- init.sql
-CREATE DATABASE "taa_db";
+CREATE DATABASE "taa_db_9tuc";
 -- Optionally create a user and grant privileges
-GRANT ALL PRIVILEGES ON DATABASE "taa_db" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "taa_db_9tuc" TO postgres;
